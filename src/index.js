1ee71f89ca2d1aa89f840d9d11b7990d8c04c344
CJ Patoilo <cjpatoilo@gmail.com>
2017-08-08 11:20:01 -0300

    Fixed code lint

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 'use strict'
 const glob = require('glob')
-const nva = require('node-version-assets')
+const NodeVersionAssets = require('node-version-assets')
 const precache = require('sw-precache')
 
 function pwaPrepare (argv) {
 	const output = argv[0] || 'dist'
 
-	glob(`${output}/**/*.{css,js}`, (error, assets) => glob(`${output}/**/*.html`, (error, grepFiles) => {
-		const nvaInstance = new nva({ assets, grepFiles })
-		nvaInstance.run()
+	glob(`${output}/**/*.{css,js}`, (error, assets) => {
+		if (error) throw error
 
-		precache.write(`${output}/service-worker.js`, {
-			staticFileGlobs: [
-				`${output}/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,txt,webapp,json}`
-			],
-			stripPrefix: output
+		glob(`${output}/**/*.html`, (error, grepFiles) => {
+			if (error) throw error
+			const nva = new NodeVersionAssets({
+				assets,
+				grepFiles
+			})
+			nva.run()
+
+			precache.write(`${output}/service-worker.js`, {
+				staticFileGlobs: [
+					`${output}/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,txt,webapp,json}`
+				],
+				stripPrefix: output
+			})
 		})
-	}))
+	})
 }
 
 module.exports = pwaPrepare
