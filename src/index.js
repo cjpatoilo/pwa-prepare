035a401ff9e6102b3d34f419117baaeab374a599
CJ Patoilo <cjpatoilo@gmail.com>
2017-08-08 23:04:08 -0300

    Automation of the service worker registry

--- src/index.js
+++ src/index.js
@@ -7,26 +7,37 @@ const uglifyjs = require('uglify-js')
 
 function pwaPrepare (argv) {
 	const output = argv[0] || 'dist'
+	const swFile = `${output}/service-worker.js`
 
 	glob(`${output}/**/*.{css,js}`, (error, assets) => {
 		if (error) throw error
 
 		glob(`${output}/**/*.html`, (error, grepFiles) => {
 			if (error) throw error
+
 			const nva = new NodeVersionAssets({
 				assets,
 				grepFiles
 			})
 			nva.run()
 
-			precache.write(`${output}/service-worker.js`, {
-				staticFileGlobs: [
-					`${output}/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,txt,webapp,json}`
-				],
+			const configPrecache = {
+				staticFileGlobs: [`${output}/**/*.{html,js,css,png,jpg,jpeg,gif,svg,ico,eot,ttf,woff,woff2,txt,webapp,json}`],
 				stripPrefix: output
-			}, () => {
-				const file = uglifyjs.minify(`${output}/service-worker.js`).code
-				fs.writeFileSync(`${output}/service-worker.js`, file)
+			}
+			precache.write(
+				swFile,
+				configPrecache,
+				() => fs.writeFileSync(swFile, uglifyjs.minify(swFile).code)
+			)
+
+			grepFiles.map(html => {
+				fs.readFile(html, 'utf-8', (error, data) => {
+					if (error) throw error
+
+					data = data.replace('</body>', `<script defer async src="service-worker.js"><script>if ('serviceWorker' in navigator && window.location.protocol === 'https:') navigator.serviceWorker.register('/service-worker.js')</script></body>`)
+					fs.writeFile(html, data, (error, data) => !error ? console.log(`${html} was saved!`) : console.error(error))
+				})
 			})
 		})
 	})
